9ef25675a7f4f375be03871cb1545a05698a3387
Shubhayu Bhowmick <shubhayubhowmick@gmail.com>
2024-04-10 19:15:19 +0530

    datatypes in javascript added

---
+++ basic/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"; //treats the js code as it's newer version
+
+
+/** dataypes -> 
+ * null
+ * undefined 
+ * string - ""
+ * boolean - true/false
+ * number - can hold numbers upto 2^53
+ * bigint - bigger numbers
+ * symbol - unique
+ * object
+ */
+
+console.log(typeof(undefined));
+console.log(typeof(null));
+
+
