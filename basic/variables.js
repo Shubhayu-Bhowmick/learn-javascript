b80e4f7be11cacc107d58023cc867722b1a6682f
Shubhayu Bhowmick <shubhayubhowmick@gmail.com>
2024-04-10 18:02:40 +0530

    Added variables in Javascript

---
+++ basic/variables.js
@@ -0,0 +1,14 @@
+
+console.log('hello world');
+
+const fruit = "apple";  // cannot change the value
+
+var veggie = "tomato";
+let car = "Porshe";
+
+/** difference between var and let is that var 
+ * is functionally scoped or globally scoped 
+ * whereas let is blocked scoped.
+ */
+
+console.table([fruit, veggie, car]);
