f5a29874f4fedfe7d4b8ca264eff2626206f0860
Shubhayu Bhowmick <shubhayubhowmick@gmail.com>
2024-04-16 13:36:33 +0530

    type conversion examples

---
+++ basic/typeconversion.js
@@ -0,0 +1,15 @@
+let fruit = "apple";
+
+let valueinNumber = Number(fruit);
+
+console.log(typeof(Number(fruit))); // output - number
+console.log(valueinNumber); // output - NaN
+
+let str1 = "";
+console.log(Boolean(str1)); // output - false
+
+let str2 = null;
+console.log(Boolean(str2)); //output - false
+
+let str3 = undefined;
+console.log(Boolean(str3)); //output - false
