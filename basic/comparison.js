d18d2e3d81c6cde8e8dabd9afd6141bd154fbdb8
Shubhayu Bhowmick <shubhayubhowmick@gmail.com>
2024-04-16 13:43:53 +0530

    comparison operations

---
+++ basic/comparison.js
@@ -0,0 +1,19 @@
+
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0); 
+console.log(null <= 0); 
+
+/** rest comparison operator treats null differently 
+ * than == operator as null is converted to zero by the rest of 
+ * the comparison operator will == does not.
+ */
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// -- 
+
+console.log("2" === 2);
